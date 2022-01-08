a95713885eb8c122637e7feed375355ab4a486b0
Gary Zhang <gzhang@primeobjects.com>
2022-01-08 11:53:46 -0800

    init

---
+++ index.ts
@@ -0,0 +1,11 @@
+//  Copyright PrimeObjects Software Inc. and other contributors <https://www.primeobjects.com/>
+// 
+//  This source code is licensed under the MIT license.
+//  The detail information can be found in the LICENSE file in the root directory of this source tree.
+
+export {
+    creatWebpPhoto,
+    processPhoto,
+    processPhotoS3SNS,
+    resizePhoto
+} from './libs/photo';
---
+++ libs/photo.ts
@@ -0,0 +1,82 @@
+
+import { getS3 } from 'douhub-helper-service';
+import sharp from "sharp";
+import stream from "stream";
+import {_track} from 'douhub-helper-util';
+
+export const processPhotoS3SNS = async (record: Record<string,any>) => {
+
+    const srcBucket = record.s3.bucket.name;
+    const srcKey = decodeURIComponent(record.s3.object.key.replace(/\+/g, " "));
+
+    if (_track) console.log({ srcBucket, srcKey });
+
+    // Sanity check: validate that source and destination are different buckets.
+    if (srcKey.indexOf('.resized.') > 0) return;
+
+    const imageType = srcKey.split('.')[srcKey.split('.').length - 1];
+
+    if (imageType.toLowerCase() !== 'gif' &&
+        imageType.toLowerCase() !== 'jpg' &&
+        imageType.toLowerCase() !== 'jpeg' &&
+        imageType.toLowerCase() !== 'png') {
+        return;
+    }
+
+    const sizes = [120, 240, 480, 960, 1440];
+   
+    const ps = [];
+    for (var i = 0; i < sizes.length; i++) {
+        ps.push(processPhoto(srcBucket, srcKey, sizes[i]));
+    }
+    await Promise.all(ps);
+};
+
+export const processPhoto = async (bucketName: string, fileName: string, size: number) =>
+{
+    return await creatWebpPhoto(bucketName, await resizePhoto(bucketName, fileName, size));
+};
+
+
+export const resizePhoto = async (bucketName: string, fileName: string, size: number, region?: string) => {
+    
+    const s3 =  getS3(region);
+   
+    const sourceStream = s3.getObject({
+        Bucket: bucketName,
+        Key: fileName
+    }).createReadStream();
+    
+    const targetStream = new stream.PassThrough();
+    const targetKey = `${fileName}.resized.${size}.jpg`;
+    const upload = s3.upload({
+        Key: targetKey,
+        Bucket: bucketName,
+        Body: targetStream,
+    }).promise();
+    sourceStream.pipe(sharp().resize(size)).pipe(targetStream);
+    await upload;
+    return targetKey;
+};
+
+
+export const creatWebpPhoto = async (bucketName: string, fileName: string, region?:string) => {
+
+    const s3 =  getS3(region);
+    const sourceStream = s3.getObject({
+        Bucket: bucketName,
+        Key: fileName
+    }).createReadStream();
+
+    const targetStream = new stream.PassThrough();
+    const targetKey = `${fileName}.webp`;
+    const upload = s3.upload({
+        Key: targetKey,
+        Bucket: bucketName,
+        Body: targetStream,
+    }).promise();
+    sourceStream.pipe(sharp().webp()).pipe(targetStream);
+    await upload;
+    return targetKey;
+};
+
